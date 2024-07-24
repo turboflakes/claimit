55ea4f710f1dfe0e77cb474c8c8af54236ed0211
paulo <paulo.rmartins@gmail.com>
2024-07-24 16:01:26 +0100

    Version bump to v0.1.5

--- dist/network_storage_worker.js
+++ dist/network_storage_worker.js
@@ -216,7 +216,7 @@ function makeClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_26(arg0, arg1, arg2) {
-    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h83a09e76c322eb5f(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h27ae347b92b6758f(arg0, arg1, addHeapObject(arg2));
 }
 
 function makeMutClosure(arg0, arg1, dtor, f) {
@@ -553,7 +553,7 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper111 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper109 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 29, __wbg_adapter_26);
         return addHeapObject(ret);
     };
