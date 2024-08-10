a355d78655c40c17c80c30027c3285b9738174e2
paulo <paulo.rmartins@gmail.com>
2024-08-11 00:55:30 +0100

    fix account balance

--- network_subscription_worker.js
+++ network_subscription_worker.js
@@ -561,8 +561,8 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2400 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 254, __wbg_adapter_26);
+    imports.wbg.__wbindgen_closure_wrapper3190 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 151, __wbg_adapter_26);
         return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_closure_wrapper10836 = function(arg0, arg1, arg2) {
