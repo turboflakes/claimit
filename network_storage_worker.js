69ad41ff683bdd322c360d509f0750a638fec51d
paulo <paulo.rmartins@gmail.com>
2024-08-15 13:37:28 +0100

    fix onboarding flow

--- network_storage_worker.js
+++ network_storage_worker.js
@@ -561,8 +561,8 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper4682 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 442, __wbg_adapter_26);
+    imports.wbg.__wbindgen_closure_wrapper464 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 253, __wbg_adapter_26);
         return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_closure_wrapper14407 = function(arg0, arg1, arg2) {
