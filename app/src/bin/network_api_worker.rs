b90a419e9c0792091afed9762791e4c14b197503
paulo <paulo.rmartins@gmail.com>
2024-08-25 15:37:31 +0100

    disable api logs and revert smoldot to v0.16.0 available from subxt v0.37

--- app/src/bin/network_api_worker.rs
+++ app/src/bin/network_api_worker.rs
@@ -2,6 +2,7 @@ use claimeer_workers::Worker;
 use yew_agent::Registrable;
 
 fn main() {
-    wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
+    // TODO: make logger to only print info logs when depployed live
+    // wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
     Worker::registrar().register();
 }
