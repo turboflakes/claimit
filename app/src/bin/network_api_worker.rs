d6f0a6b91dfb51f65367f9d8d41326dc84244ac2
paulo <paulo.rmartins@gmail.com>
2024-09-03 01:18:20 +0100

    disable log

--- app/src/bin/network_api_worker.rs
+++ app/src/bin/network_api_worker.rs
@@ -3,6 +3,6 @@ use yew_agent::Registrable;
 
 fn main() {
     // TODO: make logger to only print info logs when depployed live
-    wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
+    // wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
     Worker::registrar().register();
 }
