03be0985927f928bc6ee1ce8fedef778391fea7f
Paulo Martins <paulormart@users.noreply.github.com>
2026-05-01 12:54:35 +0100

    review comment

--- app/src/bin/api_worker.rs
+++ app/src/bin/api_worker.rs
@@ -2,8 +2,8 @@ use claimit_workers::Worker;
 use yew_agent::Registrable;
 
 fn main() {
-    // TODO: make logger to only print info logs when depployed live
-    // wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
+    // TODO: make logger to only print info logs when deployed live
+    // wasm_logger::init(wasm_logger::Config::new(log::Level::Debug));
     // tracing_wasm::set_as_global_default();
     Worker::registrar().register();
 }
